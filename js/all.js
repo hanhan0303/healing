edc44d293faa69468ab193b22117185aeaf67675
hanhan0303 <105223867+hanhan0303@users.noreply.github.com>
2022-06-18 14:31:03 +0800

    改版

--- js/all.js
+++ js/all.js
@@ -8,3 +8,26 @@ navButton.addEventListener('click', function () {
 });
 
 // navMenu的js--End
+
+//go-top按鈕
+$('document').ready(function () {
+  function showBtnCondition() {
+    console.log($(this).scrollTop());
+    if ($(this).scrollTop() > 1500) {
+      $('.go-top').fadeIn();
+    } else {
+      $('.go-top').fadeOut();
+    }
+  }
+  $(window).scroll(showBtnCondition);
+  //回到頂端按鈕-點擊之後用1秒的速度往上滑動
+  $('.go-top').click(function (event) {
+    event.preventDefault();
+    $('html,body').animate(
+      {
+        scrollTop: 0,
+      },
+      1000,
+    );
+  });
+});
